20bf4470256b026a47e1223bcfa14b27944dede6
Andrey_Kovalskiy <andrey_kovalskiy@epam.com>
2019-04-19 18:44:31 +0600

    rollback env variable setup for cleanup

--- unzip-linux64.sh
+++ unzip-linux64.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-bash setup-env-linux.sh
+export JAVA_HOME=$PWD/jre8linux64
+export JETTY_HOME=$PWD/jetty9
+
+echo "JAVA_HOME="$JAVA_HOME
+echo "JETTY_HOME="$JETTY_HOME
 
 apt-get install p7zip-full
 7z x jetty9.7z $JETTY_HOME
