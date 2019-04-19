8c299d7c6efc749292e863b950560ffe012a1c14
Andrey_Kovalskiy <andrey_kovalskiy@epam.com>
2019-04-19 18:52:42 +0600

    fixed unzip linux

--- unzip-linux64.sh
+++ unzip-linux64.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-export JAVA_HOME=$PWD/jre8linux64
-export JETTY_HOME=$PWD/jetty9
-
-echo "JAVA_HOME="$JAVA_HOME
-echo "JETTY_HOME="$JETTY_HOME
-
 apt-get install p7zip-full
-7z x jetty9.7z $JETTY_HOME
-7z x jre8linux64.7z $JAVA_HOME
+7z x jetty9.7z
+7z x jre8linux64.7z
 
-chmod +x $JAVA_HOME/bin/java
+chmod +x jre8linux64/bin/java
 
 echo Artifacts unziped
